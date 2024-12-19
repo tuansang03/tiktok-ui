672679e772596fdf93e05d722cd048d00f376651
tuansang03 <tuansngb2k3@gmail.com>
2024-12-19 18:09:34 +0700

    comment getFollowings

--- src/services/userFollowService.js
+++ src/services/userFollowService.js
@@ -1,20 +1,20 @@
-import * as httpRequest from '~/utils/httpRequest';
+// import * as httpRequest from '~/utils/httpRequest';
 
-export const getFollowings = async ({ page }) => {
-    try {
-        const token =
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90aWt0b2suZnVsbHN0YWNrLmVkdS52blwvYXBpXC9hdXRoXC9yZWdpc3RlciIsImlhdCI6MTczMzg0ODc0NywiZXhwIjoxNzM2NDQwNzQ3LCJuYmYiOjE3MzM4NDg3NDcsImp0aSI6IldWMldsWE5vcmdBalJQbzAiLCJzdWIiOjcwNjcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.q8oC7lyfsKvnzw3GgmNH4K4o7XMRb4ZScUUJy22pk1Q';
+// export const getFollowings = async ({ page }) => {
+//     try {
+//         const token =
+//             'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90aWt0b2suZnVsbHN0YWNrLmVkdS52blwvYXBpXC9hdXRoXC9yZWdpc3RlciIsImlhdCI6MTczMzg0ODc0NywiZXhwIjoxNzM2NDQwNzQ3LCJuYmYiOjE3MzM4NDg3NDcsImp0aSI6IldWMldsWE5vcmdBalJQbzAiLCJzdWIiOjcwNjcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.q8oC7lyfsKvnzw3GgmNH4K4o7XMRb4ZScUUJy22pk1Q';
 
-        const res = await httpRequest.get('me/followings', {
-            params: {
-                page,
-            },
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        return res.data;
-    } catch (error) {
-        console.log(error);
-    }
-};
+//         const res = await httpRequest.get('me/followings', {
+//             params: {
+//                 page,
+//             },
+//             headers: {
+//                 Authorization: `Bearer ${token}`,
+//             },
+//         });
+//         return res.data;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
